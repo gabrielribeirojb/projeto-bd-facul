b6c66845d7418aa7d487dbf01fb407f1c097b06e
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-11-25 19:26:44 -0300

    Domain Model - Adicionando Usuario, Comentario, Disciplina, Assunto e Aula

---
+++ projeto-bd/src/main/java/com/devsuperior/projetobd/entities/Aula.java
@@ -0,0 +1,96 @@
+package com.devsuperior.projetobd.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_aula")
+public class Aula implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private String url_video;
+	
+	@ManyToOne
+	@JoinColumn(name = "assunto_id")
+	private Assunto assunto;
+	
+	public Aula(Long id, String nome, String url_video) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.url_video = url_video;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUrl_video() {
+		return url_video;
+	}
+
+	public void setUrl_video(String url_video) {
+		this.url_video = url_video;
+	}
+
+	public Assunto getAssunto() {
+		return assunto;
+	}
+
+	public void setAssunto(Assunto assunto) {
+		this.assunto = assunto;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aula other = (Aula) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ projeto-bd/src/main/java/com/devsuperior/projetobd/entities/Usuario.java
@@ -0,0 +1,117 @@
+package com.devsuperior.projetobd.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private String email;
+	private String senha;
+	private String url_foto_perfil;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Comentario> comentarios = new ArrayList<>();
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public Usuario(Long id, String nome, String email, String senha, String url_foto_perfil) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.url_foto_perfil = url_foto_perfil;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String getUrl_foto_perfil() {
+		return url_foto_perfil;
+	}
+	
+	public void setUrl_foto_perfil(String url_foto_perfil) {
+		this.url_foto_perfil = url_foto_perfil;
+	}
+
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+	
+}
